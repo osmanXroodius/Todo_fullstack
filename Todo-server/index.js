ae2f9a1c6e2c13f90a74f1b8a46775f1eb7114c7
OsmanXwiinder <osmansaifi30@gmail.com>
2025-04-19 20:41:05 +0530

    all-set

---
+++ Todo-server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.port
+console.log(port)
+
+
+app.use('user',)
+app.use('todo',)
+
+
+
+
+
+
+app.listen(port, () => console.log(`server Running on port ${port}`))
+
+
+
+
+
+
+
+
