3aff53e39ea65512e7fcaf939f0125a3eb53bd28
OsmanXwiinder <osmansaifi30@gmail.com>
2025-04-23 15:19:40 +0530

    server completed

--- Todo-server/routes/todo.js
+++ Todo-server/routes/todo.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usermiddleware = require('../middleware/user')
 const z = require('zod');
 const { TodoDb } = require('../db/db');
+const { json } = require('body-parser');
 
 const todoInputSchema = z.object({
     title:z.string(),
@@ -33,18 +34,52 @@ router.get('/Gettodo',usermiddleware ,async (req,res) => {
     })
 })
 
-router.get('/Gettodo/:id',usermiddleware, (req,res) => {
+router.get('/Gettodo/:id',usermiddleware,async (req,res) => {
+    const TodoID  = req.params.id;
+    // const Newid  = TodoID.toString();
+    const findById = await TodoDb.findById(TodoID)
+    
+    res.status(200).json({YourTodo : findById});
+})
+ 
+router.put('/Updatetodo/:id',usermiddleware, async (req,res) => {
+    const TodoID = req.params.id;
+    const title = req.body.title
+    const description = req.body.description
+
+    try{
+const Updatedtodo = await TodoDb.findByIdAndUpdate(TodoID,{
+        title:title,
+        description:description
+    },{new:true, runValidators:true})
+    
+    if(!Updatedtodo){
+        return res.status(403).json({msg:"Todo not Found"})
+    }   
+    res.status(200).json({Updatedtodo})
+    }catch(err){
+        console.log(err)
+        res.status(500).json({msg:err.message})
+    }
+
+})
+
+
+router.delete('/DeleteTodo/:id',usermiddleware,async (req,res) => {
+    const TodoID = req.params.id
+
+    try{
+        const deleted = await TodoDb.findByIdAndDelete(TodoID)
+        if(!deleted){
+            return res.status(402).json({msg:"Not Deleted"})
+        }else {
+            res.status(200).json({mag:"Deleted"})
+        }
+    }catch(err){
+        res.status(500).json({err:err.message})
+    }
     
 })
 
-router.patch('/Updatetodo',usermiddleware, (req,res) => {
-    
-})
 
-
-router.delete('/DeleteTodo',usermiddleware, (req,res) => {
-
-})
-
-
-module.exports = router
+module.exports = router;
